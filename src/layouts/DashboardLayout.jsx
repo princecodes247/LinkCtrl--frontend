94e76e7be203625ad3f0954d4626a2d34b8a620e
Prince Codes <princecodes247@gmail.com>
2022-07-13 19:28:59 -0400

    Update Dashboard Layout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 // import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { DashboardHeader } from '../components/Header';
+import Footer from '../components/Footer';
 
 function DashboardLayout({ children }) {
   const params = useParams();
   return (
-    <main className="flex flex-col md:flex-row justify-center md:h-screen">
-      <section className=" flex-1">{params.auth}</section>
-      <section className="flex-1 flex flex-col text-white bg-gray-800 p-4 justify-center items-center md:items-stretch">
-        {children}
-      </section>
-    </main>
+    <>
+      <DashboardHeader />
+      <main className="text-white">{children}</main>
+      <Footer />
+    </>
   );
 }
 
