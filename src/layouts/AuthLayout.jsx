77a29da0c5679e5d11b5c27b8adf019209f94fd9
Prince Codes <princecodes247@gmail.com>
2022-07-13 18:41:20 -0400

    Update layouts

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+// import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function AuthLayout({ children }) {
+  const params = useParams();
+  return (
+    <main className="flex flex-col md:flex-row justify-center md:h-screen">
+      <section className=" flex-1">{params.auth}</section>
+      <section className="flex-1 flex flex-col text-white bg-gray-800 p-4 justify-center items-center md:items-stretch">
+        {children}
+      </section>
+    </main>
+  );
+}
+
+AuthLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthLayout;
