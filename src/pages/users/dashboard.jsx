e1d4177a8da4fde23a14766518512185a5126e24
Prince Codes <princecodes247@gmail.com>
2022-07-13 19:29:18 -0400

    Implement DashboardLayout

--- src/pages/users/dashboard.jsx
+++ src/pages/users/dashboard.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 // import React, { useState } from 'react';
+import { DashboardLayout } from '../../layouts';
+import Button from '../../components/Button';
+import ProjectCard from '../../components/ProjectCard';
 
 function Dashboard() {
   return (
-    <footer className="flex justify-center p-4">
-      <Link to="/">Expenses</Link>
-      <div className="App-header">LinkCtrl footer</div>
-    </footer>
+    <DashboardLayout>
+      <nav className="flex justify-between p-4">
+        <h2>All Projects</h2>
+        <div>Filter</div>
+      </nav>
+      <section>
+        {/* <div>
+          <p>No projects found</p>
+          <Button>Create new project</Button>
+        </div> */}
+        <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 border border-red-400">
+          {[1, 2, 3, 4, 4, 4, 4, 4].map((projectID) => (
+            <ProjectCard projectID={projectID} />
+          ))}
+        </div>
+      </section>
+    </DashboardLayout>
   );
 }
 
