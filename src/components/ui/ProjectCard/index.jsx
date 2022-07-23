56ada858d6b781dc9f4152715a8a58232fc91ae6
Prince Codes <princecodes247@gmail.com>
2022-07-23 16:51:33 -0400

    Update Project Card

--- src/components/ui/ProjectCard/index.jsx
+++ src/components/ui/ProjectCard/index.jsx
@@ -2,12 +2,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+// import LinkIcon from '../../../assets/LinkIcon1.svg';
 
 function ProjectCard({ projectID }) {
   return (
     <Link to={`/project/${projectID}`}>
-      <div className="p-4 flex justify-center">
-        <p>Name</p>
+      <div className="bg-gray-900 bg-opacity-20 hover:bg-opacity-40 p-4 flex flex-col justify-center rounded transition duration-100">
+        <div className="flex items-center justify-between">
+          <h3>Ebi Campaign</h3>
+          <div className="flex flex-col items-center">
+            <h4>12</h4>
+            <p className="text-xs text-gray-600">click/hr</p>
+          </div>
+        </div>
+        <div className="text-sm">
+          <p className="text-gray-700">https://www.google.com</p>
+          {/* <LinkIcon /> */}
+          <p className="text-gray-400">https://www.google.com</p>
+        </div>
+        {/* <div className="my-4">Lorem ipsum dolor sit amet.</div> */}
+        <div className="flex whitespace-nowrap gap-2 mt-4 justify-between text-sm">
+          <div className="flex-1 text-center bg-gray-900 rounded px-2">
+            <p className="text-xs">total clicks</p>
+            <p className="text-base">78</p>
+          </div>
+          <div className="flex-1 text-center bg-gray-900 rounded px-2">
+            <p className="text-xs">Expires in</p>
+            <p className="text-base">43m</p>
+          </div>
+          <div className="flex-1 text-center bg-gray-900 rounded px-2">
+            <p className="text-xs">clicks left</p>
+            <p className="text-base">4</p>
+          </div>
+        </div>
       </div>
     </Link>
   );
--- src/pages/users/dashboard.jsx
+++ src/pages/users/dashboard.jsx
@@ -12,16 +12,18 @@ function Dashboard() {
   return (
     <DashboardLayout>
       <div className="w-full flex items-center justify-center">
-        <div className="flex w-full gap-2 mx-12">
+        <div className="flex w-full flex-col md:flex-row gap-2 mx-12">
           <input
-            className="p-3 gap-4 rounded flex-1 bg-gray-900"
+            className="p-3 rounded flex-1 bg-opacity-50 bg-gray-900"
             type="text"
-            placeholder="Search projects..."
+            placeholder="Paste a link to shorten it..."
           />
-          <Button variant="secondary" onClick={() => {}}>
-            Quick Shorten
-          </Button>
-          <Button onClick={() => setIsModalOpen(true)}>Shorten it</Button>
+          <div className="flex gap-2  justify-end">
+            <Button variant="secondary" onClick={() => {}}>
+              Quick Shorten
+            </Button>
+            <Button onClick={() => setIsModalOpen(true)}>Shorten it</Button>
+          </div>
           {/* <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} /> */}
           <ConfigureLinkModal
             isOpen={isModalOpen}
@@ -41,7 +43,7 @@ function Dashboard() {
           <p>No projects found</p>
           <Button>Create new project</Button>
         </div> */}
-        <div className="flex flex-col gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-8 px-4">
+        <div className="flex flex-col gap-8 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-8 px-4">
           {[1, 2, 3, 4, 4, 4, 4, 4].map((projectID) => (
             <ProjectCard projectID={projectID} />
           ))}
