8afa2e31b42375d74e225f9487c466dc2afaf921
Prince Codes <princecodes247@gmail.com>
2022-07-23 16:16:29 -0400

    Update button styles

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -2,15 +2,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
-function Button({ to, children }) {
+const styles = (override, variant, outline, classNames) =>
+  clsx([
+    !override && [
+      variant === 'secondary'
+        ? [
+            outline
+              ? 'border border-white'
+              : 'bg-gray-600 bg-opacity-60 hover:bg-opacity-80',
+          ]
+        : 'bg-green-400 hover:brightness-110',
+      'px-8 py-3 rounded text-center text-white active:brightness-75',
+    ],
+    classNames,
+  ]);
+function Button({
+  to,
+  override,
+  variant,
+  outline,
+  classNames,
+  children,
+  onClick,
+}) {
+  // override, variant, outline, classNames
   // const { isLink } = props;
   return to.length > 0 ? (
-    <Link className="bg-green-400 p-5 text-center py-3 flex-1" to={`/${to}`}>
+    <Link
+      className={styles(override, variant, outline, classNames)}
+      to={`/${to}`}
+      onClick={onClick}
+    >
       {children}
     </Link>
   ) : (
-    <button className="bg-red-400 text-center p-5 py-3 flex-1" type="button">
+    <button
+      className={styles(override, variant, outline, classNames)}
+      type="button"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
