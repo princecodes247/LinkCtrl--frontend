ed65b0ab08628f96bce9f945b273e558a0c9b599
Prince Codes <princecodes247@gmail.com>
2022-07-26 02:27:55 -0400

    Add overflow to Modal Wrapper

--- src/components/ui/Modal/ModalWrapper.jsx
+++ src/components/ui/Modal/ModalWrapper.jsx
@@ -4,7 +4,7 @@ import Button from '../Button';
 
 function ModalWrapper({ modalRef, children }) {
   return (
-    <div className="fixed flex items-center top-0 left-0 h-screen w-screen flex item-center justify-center bg-opacity-80 bg-black">
+    <div className="fixed flex items-center top-0 left-0 h-screen w-screen overflow-auto flex item-center justify-center bg-opacity-80 bg-black">
       <div className="bg-gray-900 p-9 rounded" ref={modalRef}>
         {children}
       </div>
